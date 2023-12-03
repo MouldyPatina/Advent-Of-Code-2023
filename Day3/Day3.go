0634f62766e220d76dbf787cd0be018ead1a7b66
MouldyPatina <138029901+MouldyPatina@users.noreply.github.com>
2023-12-03 12:35:27 +0000

    Solution to Part 1

--- Day3/Day3.go
+++ Day3/Day3.go
@@ -16,17 +16,47 @@ func init () {
 }
 
 func main() {
-	calibrations := readFile("Test");
-	for _, line := range calibrations {
-		fmt.Println(line)
+	calibrations := readFile("Real");
+	fmt.Println(Part1Solver(calibrations));
+}
+
+func Part1Solver(calibration []string) int {
+	sum := 0;
+	for j := 0; j < len(calibration); j++ {
+		x := [2]int{-1, -1};
+		y := j;
+		temp := 0;
+		for i := 0; i < len(calibration[j]); i++ {
+			if letter := calibration[j][i]; letter >= '0' && letter <= '9' {
+				temp = temp * 10 + int(letter) - int('0');
+				if x[0] == -1 {
+					x[0] = i;
+					x[1] = i;
+				} else {
+					x[1] = i;
+				}
+			} else if x[0] != -1 && x[1] != -1 {
+				if NearSymbol(x, y, calibration) {
+					sum += temp;
+				}
+				x = [2]int{-1, -1};
+				temp = 0;
+			}
+		}
+		if x[0] != -1 && x[1] != -1 {
+			if NearSymbol(x, y, calibration) {
+				sum += temp;
+			}
+		}
 	}
+	return sum;
 }
 
 func NearSymbol(x [2]int, y int, calibration []string) bool {
-	for j := Max(0, y - 1); j < Min(len(calibration, y + 1); j++ {
-		for i := Max(0, x[0] - 1); i < Min(len(calibration[j]), x[1] + 1); i++ {
-			if j == y && i >= x[0] && i <= x[1] {}
-			else if word := calibration[j][i]; (word > '9' || word < '0') && word != '.' {
+	for j := Max(0, y - 1); j < Min(len(calibration), y + 2); j++ {
+		for i := Max(0, x[0] - 1); i < Min(len(calibration[j]), x[1] + 2); i++ {
+			if j == y && i >= x[0] && i <= x[1] {
+			} else if word := calibration[j][i]; (word > '9' || word < '0') && word != '.' {
 				return true;
 			}
 		}
