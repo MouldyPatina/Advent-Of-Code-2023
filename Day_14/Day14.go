742b5de98be1eaec9ab04dcbfd65a8e6abf8f1f7
MouldyPatina <138029901+MouldyPatina@users.noreply.github.com>
2023-12-17 22:00:31 +0000

    Solution to Part 2 of Day 14
    Made a function to tilt platform, read instructions to remove an extra north tilt I was doing, and cached the results from each cycle so I could find the looping pattern and not have to cycle 1000000000 times

--- Day_14/Day14.go
+++ Day_14/Day14.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	platform := readFile("Test")
+	platform := readFile("Real")
 	fmt.Println("This is the solution to Part 1:")
 	fmt.Println(Part1Solver(platform))
 	fmt.Println("This is the solution to Part 2:")
@@ -38,7 +38,20 @@ func Part2Solver(platform []string) int {
 	for j := 0; j < len(cyclePlatform); j++ {
 		cyclePlatform[j] = strings.Split(platform[j], "");
 	}
-	cyclePlatform = Cycled(cyclePlatform);
+	cache := make(map[string] int);	
+	cache[ToString(cyclePlatform)] = 0;
+	for k := 0; k < 1000000000; k++ {
+		Cycled(cyclePlatform);
+		if cache[ToString(cyclePlatform)] == 0 {
+			cache[ToString(cyclePlatform)] = k;
+		} else {
+			first := cache[ToString(cyclePlatform)];
+			for t := 0; t < (1000000000 - first - 1) % (k - first); t++ {
+				Cycled(cyclePlatform);
+			}
+			break;
+		}
+	}
 	var totalLoad = 0;
 	for i := 0; i < len(cyclePlatform[0]); i++ {
 		rockLoad := len(cyclePlatform);
@@ -47,7 +60,7 @@ func Part2Solver(platform []string) int {
 				case "#":
 					rockLoad = len(cyclePlatform) - j - 1;
 				case "O":
-					totalLoad += rockLoad;
+					totalLoad += len(cyclePlatform) - j;
 					rockLoad--;
 			}
 		}
@@ -55,9 +68,94 @@ func Part2Solver(platform []string) int {
 	return totalLoad;
 }
 
-func Cycled(platform [][]string) [][]string {
-	rotatedPlatform := make([][]string, len(platform));
+func ToString(matrix [][]string) string {
+	var output = "";
+	for j := 0; j < len(matrix); j++ {
+		for i := 0; i < len(matrix[j]); i++ {
+			output += matrix[j][i];
+		}
+	}
+	return output;
 }
+
+func Cycled(platform [][]string) {
+	TiltUp(platform);
+	TiltRight(platform);
+	TiltDown(platform);
+	TiltLeft(platform);
+	temp := "";
+	for _, line := range platform {
+		for _, rock:= range line {
+			temp += rock
+		}
+		temp += "\n";
+	}
+}
+
+func TiltUp(platform [][]string) {
+	for i := 0; i < len(platform[0]); i++ {
+		rollUp := 0;
+		for j := 0; j < len(platform); j++ {
+			switch platform[j][i] {
+			case "#":
+				rollUp = j + 1;
+			case "O":
+				platform[j][i] = ".";
+				platform[rollUp][i] = "O";
+				rollUp++;
+			}
+		}
+	}
+}
+
+func TiltRight(platform [][]string) {
+	for j := 0; j < len(platform); j++ {
+		rollUp := 0;
+		for i := 0; i < len(platform[j]); i++ {
+			switch platform[j][i] {
+			case "#":
+				rollUp = i + 1;
+			case "O":
+				platform[j][i] = ".";
+				platform[j][rollUp] = "O";
+				rollUp++;
+			}
+		}
+	}
+}
+
+func TiltDown(platform [][]string) {
+	for i := 0; i < len(platform[0]); i++ {
+		rollUp := len(platform) - 1;
+		for j := len(platform) - 1; j > -1; j-- {
+			switch platform[j][i] {
+			case "#":
+				rollUp = j - 1;
+			case "O":
+				platform[j][i] = ".";
+				platform[rollUp][i] = "O";
+				rollUp--;
+			}
+		}
+	}
+}
+
+func TiltLeft(platform [][]string) {
+	for j := 0; j < len(platform); j++ {
+		rollUp := len(platform[j]) - 1;
+		for i := len(platform[j]) - 1; i > -1; i-- {
+			switch platform[j][i] {
+			case "#":
+				rollUp = i - 1;
+			case "O":
+				platform[j][i] = ".";
+				platform[j][rollUp] = "O";
+				rollUp--;
+			}
+		}
+	}
+}
+
 
 func init() {
 	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
